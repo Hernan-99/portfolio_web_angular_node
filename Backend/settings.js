e6ebc5d1b7105d3b6569768a21b17201d1267b83
HernanGabriel <hernan24744@gmail.com>
2023-07-02 22:12:56 -0300

    rama release actualizada

--- Backend/settings.js
+++ Backend/settings.js
@@ -3,8 +3,9 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB__USER = process.env.DB__USER || "root";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "admin1234";
+export const DB_HOST =
+  process.env.DB_HOST || "b05klvbeslslyfcggndd-mysql.services.clever-cloud.com";
+export const DB__USER = process.env.DB__USER || "u2kxeaivcvz8qyzm";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "5o6isce6Ed1qQqPkZ0j4";
 export const DB_PORT = process.env.DB_PORT || 3306;
-export const DB_DATABASE = process.env.DB_DATABASE || "portfoliodb";
+export const DB_DATABASE = process.env.DB_DATABASE || "b05klvbeslslyfcggndd";
